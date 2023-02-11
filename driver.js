fe785e2adb0643a19a2c245cb5fe20c0ba9b41ea
Bedrockdude10 <dannyrollo4@gmail.com>
2023-02-11 16:35:26 -0500

    pages updates

--- driver.js
+++ driver.js
@@ -1,26 +1,25 @@
-
+const neo4j = require('neo4j-driver')
 
 const uri = "bolt+s://4880961c56ecb6059750f38a0db86f46.neo4jsandbox.com:7687"
 const user = "neo4j"
 const password = "carrier-pea-swells"
 const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
 const session = driver.session()
+const personName = 'Alice'
 
-// try {
-//     const result = await session.executeWrite(tx =>
-//         tx.run(
-//             'MATCH (a) RETURN a',
-//             { message: 'hello, world' }
-//         )
-//     )
-//
-//     const singleRecord = result.records[0]
-//     const greeting = singleRecord.get(0)
-//
-//     console.log(greeting)
-// } finally {
-//     await session.close()
-// }
-//
-// // on application exit:
-// await driver.close()
+try {
+    const result = await session.run(
+        'CREATE (a:Person {name: $name}) RETURN a',
+        { name: personName }
+    )
+
+    const singleRecord = result.records[0]
+    const node = singleRecord.get(0)
+
+    console.log(node.properties.name)
+} finally {
+    await session.close()
+}
+
+// on application exit:
+await driver.close()
