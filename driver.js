debec14ba6420f413f48dc7d29d83957d330264f
Bedrockdude10 <dannyrollo4@gmail.com>
2023-02-11 16:51:51 -0500

    Added neo4j driver as a dependency (see package.json & package-lock.json)

--- driver.js
+++ driver.js
@@ -7,19 +7,19 @@ const driver = neo4j.driver(uri, neo4j.auth.basic(user, password))
 const session = driver.session()
 const personName = 'Alice'
 
-try {
-    const result = await session.run(
-        'CREATE (a:Person {name: $name}) RETURN a',
-        { name: personName }
-    )
-
-    const singleRecord = result.records[0]
-    const node = singleRecord.get(0)
-
-    console.log(node.properties.name)
-} finally {
-    await session.close()
-}
-
-// on application exit:
-await driver.close()
+// try {
+//     const result = await session.run(
+//         'CREATE (a:Person {name: $name}) RETURN a',
+//         { name: personName }
+//     )
+//
+//     const singleRecord = result.records[0]
+//     const node = singleRecord.get(0)
+//
+//     console.log(node.properties.name)
+// } finally {
+//     await session.close()
+// }
+//
+// // on application exit:
+// await driver.close()
